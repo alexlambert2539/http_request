55aa976cd05ebe62cc0568f15dad292b10ae4466
alexlambert2539 <alexlambert.eng@gmail.com>
2017-09-15 21:26:27 +0100

    Initial commit of http_request

---
+++ http_request/src/http_request/Main.java
@@ -0,0 +1,13 @@
+package http_request;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		SimpleClass simpleClass = new SimpleClass();
+		
+		simpleClass.display();
+		
+	}
+	
+}
---
+++ http_request/src/http_request/SimpleClass.java
@@ -0,0 +1,11 @@
+package http_request;
+
+public class SimpleClass {
+
+	public void display() {
+		
+		System.out.println("simple class output string");
+		
+	}
+	
+}
