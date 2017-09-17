d3fcd358611367558d71c7502c71267de2f6a9c6
alexlambert2539 <alexlambert.eng@gmail.com>
2017-09-17 18:37:23 +0100

    Crude loop to allow program to run constantly

--- http_request/src/http_request/Main.java
+++ http_request/src/http_request/Main.java
@@ -7,28 +7,36 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		ReadInput readInput = new ReadInput();
-		Request request = new Request();
-		JsonFormat jsonFormat = new JsonFormat();
-		
-		ArrayList<String> arrayList = readInput.readInput();
-		
-		for (int i=0; i<arrayList.size(); i++) {
-						
-			try {
-				
-				RequestProperties requestProperties = request.getRequest(arrayList.get(i));
-				
-				System.out.println(jsonFormat.toJson(requestProperties));
-				
-			} catch (IOException e) {
-				
-				System.err.println("unable to perform GET request: " + arrayList.get(i));
+		while(true) {
+			
+			ReadInput readInput = new ReadInput();
+			Request request = new Request();
+			JsonFormat jsonFormat = new JsonFormat();
+			
+			System.out.println("enter list of public web addresses");
+			
+			ArrayList<String> arrayList = readInput.readInput();
+			
+			for (int i=0; i<arrayList.size(); i++) {
+							
+				try {
+					
+					RequestProperties requestProperties = request.getRequest(arrayList.get(i));
+					
+					System.out.println(jsonFormat.toJson(requestProperties));
+					
+				} catch (IOException e) {
+					
+					System.err.println("unable to perform GET request: " + arrayList.get(i));
+					
+				}
 				
 			}
 			
+			System.out.println("finished");
+			
 		}
-		
+					
 	}
 	
 }
