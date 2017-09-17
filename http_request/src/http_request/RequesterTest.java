04abc4b0b16c5aa7b1fafdd1f8c275f2fbbb2bf6
alexlambert2539 <alexlambert.eng@gmail.com>
2017-09-17 17:09:44 +0100

    Changes to requester test class to match requestProperties

--- http_request/src/http_request/RequesterTest.java
+++ http_request/src/http_request/RequesterTest.java
@@ -3,12 +3,11 @@ package http_request;
 import static org.junit.Assert.*;
 
 import java.io.IOException;
-import java.net.URL;
 import org.junit.Test;
 
 public class RequesterTest {
 	
-	private String[] urlStringName = {"https://www.bbc.co.uk/iplayer", 
+	private String[] urlStringName = {"http://www.bbc.co.uk/iplayer", 
 	                                  "https://google.com", 
 	                                  //"bad://address", 
 	                                  "http://www.bbc.co.uk/missing/thing", 
@@ -16,28 +15,22 @@ public class RequesterTest {
 	                                  "http://www.oracle.com/technetwork/java/javase/downloads/index.html", 
 	                                  "https://www.pets4homes.co.uk/images/articles/1646/large/kitten-emergencies-signs-to-look-out-for-537479947ec1c.jpg", 
 	                                  "http://site.mockito.org/"};
-
+	
 	@Test
 	public void test() throws IOException {
 		
+		Requester requester = new Requester();
+		
 		for (int i=0; i<urlStringName.length; i++) {
 			
-			Requester requester = new Requester();
-			requester.getRequest(urlStringName[i]);
-			URL url = requester.getUrl();
-			int statusCode = requester.getStatusCode();
-			int contentLength = requester.getContentLength();
-			String date = requester.getDate();
+			RequestProperties requestProperties = requester.getRequest(urlStringName[i]);		
 			
-			assertEquals(url, new URL(urlStringName[i]));
-			assertNotNull(statusCode);
-			assertNotNull(contentLength);
-			assertNotNull(date);
+			assertEquals(requestProperties.url, urlStringName[i]);
 			
-			System.out.println(url);
-			System.out.println(statusCode);
-			System.out.println(contentLength);
-			System.out.println(date);
+			System.out.println(requestProperties.url);
+			System.out.println(requestProperties.statusCode);
+			System.out.println(requestProperties.contentLength);
+			System.out.println(requestProperties.date);
 			
 		}
 		
